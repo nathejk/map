b4990c140b077ebf4b5bda33c9e4523d5c2d37a9
Asbjørn Thegler <asbjoern@gmail.com>
2016-09-01 15:52:45 +0200

    Add clock

--- public/js/map.js
+++ public/js/map.js
@@ -244,4 +244,21 @@ clearSearchElement.onclick = clearSearchQuery;
 initLayers(function() {
     update();
     window.setInterval(update, 5000);
+    startTime()
 });
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
